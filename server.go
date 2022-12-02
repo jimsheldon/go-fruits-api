0b230a26c78172ead64422bac5ecd81dd21c82ca
Jim Sheldon <james.sheldon@harness.io>
2022-12-02 16:26:49 -0500

    Update server.go

--- server.go
+++ server.go
@@ -32,6 +32,8 @@ var (
 	router         *echo.Echo
 )
 
+// trigger
+
 // @title Fruits API
 // @version 1.0
 // @description The Fruits API that defines few REST operations with Fruits used for demos
